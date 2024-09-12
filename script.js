5807d318e717a540933c90d727f2814c497381d1
Deepak9123 <deepakmahatoj.n.v@gmail.com>
2024-09-13 01:32:40 +0530

    added footer

--- script.js
+++ script.js
@@ -6,30 +6,24 @@ const modal = document.getElementById("contactModal");
 const btn = document.getElementById("hireMeBtn");
 const span = document.getElementsByClassName("close")[0];
 
-// Function to update the active link based on scroll position
 function updateActiveLink() {
-    let index = sections.length; // Default index
-
-    // Loop through sections to find which one is in view
+    let index = sections.length; 
     while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
     navbarLinks.forEach((link) => link.classList.remove('active'));
     navbarLinks[index].classList.add('active');
 }
 
-// Toggle the visibility of the navbar on hamburger click
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
-    navbar.classList.toggle('show'); // Ensure the class for visibility is 'show'
+    navbar.classList.toggle('show'); 
 });
 
-// Smooth scrolling and close the mobile menu on link click
 navbarLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
 
         hamburger.classList.remove('active');
-        navbar.classList.remove('show'); // Close the menu on click
+        navbar.classList.remove('show'); 
 
         const targetId = link.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
@@ -42,13 +36,9 @@ navbarLinks.forEach(link => {
     });
 });
 
-// Add event listener for scrolling to update the active link
 window.addEventListener('scroll', updateActiveLink);
-
-// Initial call to set the active link on page load
 updateActiveLink();
 
-// Modal functionality
 btn.onclick = function() {
     modal.style.display = "block";
 }
